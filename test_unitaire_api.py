fea55c5de25f65798979680fb8473c0b5b0bb8a1
KAHLOUCHE <kahlouchesoraya27@gmail.com>
2024-06-12 21:59:28 +0200

    ajout du nouveau des modif des test unitaire

--- test_unitaire_api.py
+++ test_unitaire_api.py
@@ -1,102 +1,84 @@
 # Import packages
 import unittest
-import requests
-import json
+from app import app  # Importez votre application Flask directement depuis le fichier app.py
 
 # Global variables definition
-api_url = "http://13.38.104.45:5000"
-
 n_clients = 1000
 
 # API tests
 class TestDefaulPredictAPI(unittest.TestCase):
 
+    def setUp(self):
+        # Créez un client de test Flask pour simuler les requêtes HTTP
+        self.app = app.test_client()
+
     # Test # 1
     def test_welcome(self):
-
-        response = requests.get(api_url)
-
-        # Test # 1.1: status code = 200
+        # Effectuez une requête GET à l'endpoint racine de l'API
+        response = self.app.get('/')
+        
+        # Testez la réponse
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode('utf-8'), "Hello world! Welcome to the falask API!")
 
-        # Test # 1.2: The response gives the right message
-        real_res = json.loads(response.content)
-        expected_res = "Hello world! Welcome to the falask API!"
-        self.assertEqual(real_res, expected_res)
-    
     # Test # 2
     def test_load_client_list(self):
+        # Effectuez une requête GET à l'endpoint '/client_list' de l'API
+        response = self.app.get('/client_list')
         
-        response = requests.get(api_url + '/client_list')
-        
+        # Testez la réponse
         self.assertEqual(response.status_code, 200)
 
-        # Test # 2.2: the output is a list
+        # Testez le format de la réponse
         id_list = response.json()
         self.assertIsInstance(id_list, list)
+        self.assertEqual(len(id_list), n_clients)
+        self.assertEqual(id_list[0], 438333)  # Vérifiez le premier ID de la liste
 
-        # Test # 2.3: the output list has the right lenght
-        len_id_list = len(id_list)
-        self.assertEqual(len_id_list, n_clients)
-
-        # Test # 2.4: the first id in the list is the right one
-        first_id = 438333
-        self.assertEqual(id_list[0], first_id)
-    
     # Test # 3
     def test_load_client(self):
-        
         client_id = 438333
+        # Effectuez une requête GET à l'endpoint '/client' avec l'ID du client
+        response = self.app.get(f'/client?id={client_id}')
         
-        response = requests.get(api_url + f'/client?id={client_id}')
-        
-        # Test # 3.1: status code = 200
+        # Testez la réponse
         self.assertEqual(response.status_code, 200)
 
-        # Test # 3.2: the client data are correct
+        # Testez les données du client
         client = response.json()
         self.assertEqual(client['AMT_ANNUITY'], 19908.0)
         self.assertEqual(client['AMT_CREDIT'], 252000.0)
-        self.assertEqual(client['AMT_INCOME_TOTAL'], 315000.0 )
+        self.assertEqual(client['AMT_INCOME_TOTAL'], 315000.0)
         self.assertEqual(client['DAYS_BIRTH'], 29)
 
     # Test # 4
     def test_load_data(self):
-        
         col = 'DAYS_BIRTH'
+        # Effectuez une requête GET à l'endpoint '/data' avec la colonne spécifiée
+        response = self.app.get(f'/data?col={col}')
         
-        response = requests.get(api_url + f'/data?col={col}')
-        
-        # Test # 4.1: status code = 200
+        # Testez la réponse
         self.assertEqual(response.status_code, 200)
 
-        # Test # 4.2: the output is a list
+        # Testez le format des données
         age_list = response.json()
         self.assertIsInstance(age_list, list)
-
-        # Test # 4.3: the output list has the right lenght
-        len_age_list = len(age_list)
-        self.assertEqual(len_age_list, n_clients)
-
-        # Test # 4.4: the first id in the list is the right one
-        first_age = 29.85753424657534
-        self.assertEqual(age_list[0], first_age)
+        self.assertEqual(len(age_list), n_clients)
+        self.assertAlmostEqual(age_list[0], 29.85753424657534)  # Utilisez `assertAlmostEqual` pour les valeurs flottantes
 
     # Test # 5
-    def test_predict_defaul(self):
-
+    def test_predict_default(self):
         client_id = 438333
+        # Effectuez une requête GET à l'endpoint '/predict_default' avec l'ID du client
+        response = self.app.get(f'/predict_default?id_client={client_id}')
         
-        response = requests.get(api_url + f'/predict_default?id_client={client_id}')
-
-        # Test # 5.1: status code = 200
+        # Testez la réponse
         self.assertEqual(response.status_code, 200)
 
-        # Test # 5.2: the ptrediction is correct (Once the model re-compiled, test values have to be updated)
+        # Testez la prédiction
         prediction = response.json()
-        self.assertEqual(prediction['proba_0'], 0.0022691398703050814)
-        self.assertEqual(prediction['proba_1'], 0.9977308601296949)
+        self.assertAlmostEqual(prediction['proba_0'], 0.0022691398703050814)
+        self.assertAlmostEqual(prediction['proba_1'], 0.9977308601296949)
 
 if __name__ == '__main__':
     unittest.main()
-    
